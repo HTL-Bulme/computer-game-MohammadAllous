0072b3f8cc2544273ac1317d3a40714f0deae4b4
mohammad.alloush@bulme.at <mohammad.alloush@debian-BULLSEYE-live-builder-AMD64>
2024-02-29 17:30:34 +0100

    probe

---
+++ Pygame_intro.py
@@ -0,0 +1,10 @@
+x = int(input("enter an number"))
+if x < 0:
+    x=0
+    print("Negativ Pleas enter an neu number")
+elif x==0:
+    x = int(input("enter an number"))
+if x < 0:
+    x=0
+    print("Negativ Pleas enter an neu number")
+elif x==0:
